2b6d59a029aa43b7f41011d78664d1a7cd321aa6
Kenjiro Aguilera <kenjiro.mendez@gmail.com>
2025-11-23 01:20:10 -0300

    cambio de nombre

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.relojcontrol"
+        applicationId = "cl.soydev.relojcontrol"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
